966ba5e6416c2ea1d42e1b49f69279c0a1cf14ed
Seth Bowman <brandonsethbowman@gmail.com>
2024-02-16 10:31:04 -0600

    Add project files.

---
+++ MethodsDemoCSharp-45/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace MethodsDemoCSharp_45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calling a method (using a method)
+            GreetPerson("Seth");
+            GreetPerson("Mack");
+
+            int ourAdd = Add(2, 2);
+            int ourAddTwo = Add(3, 3);
+            Console.WriteLine(ourAdd);
+            Console.WriteLine(ourAddTwo);
+
+            GetUserNameAndGreet();
+        }
+
+        //Method definition (creating a method)
+        //Parts of a method
+        //Disregard the keyword "static" for now
+        //Pascal casing for methods - EveryNextWordStartsWithCapital
+        //Access Modifier - Return type - Method name - Parameter list - Scope (body)
+        public static void GreetPerson(string name)
+        {
+            Console.WriteLine($"Hey, {name}!");
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static void GetUserNameAndGreet()
+        {
+            Console.WriteLine("Please enter your name:");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hey, {name}!");
+        }
+
+        //Adding operator 2 + 2
+        //Subtract 2 - 2
+        //Dividing 2 / 2
+        //Multiply 2 * 2
+    }
+}
